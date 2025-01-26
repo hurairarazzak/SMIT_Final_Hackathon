1168ba2eb561b4f55076b5744b518225ca092090
hurairarazzak <hurairarazzak125@gmail.com>
2025-01-27 01:47:29 +0500

    endpoints updated

--- frontend_smitfinalhackathon/src/routes/routes.js
+++ frontend_smitfinalhackathon/src/routes/routes.js
@@ -1,5 +1,5 @@
 const devUrl = "http://localhost:4000/";
-const prodUrl = "https://smit-final-hackathon-backend-livid.vercel.app/"; // Add the backend production URL here
+const prodUrl = "https://smit-final-hackathon-backend-livid.vercel.app"; // Add the backend production URL here
 
 // Set BASE_URL depending on the environment
 export const BASE_URL = prodUrl;
